8e3a585ea3d4feec70eae4c2099a8c56857e44f3
Robert Burnett <dr.slate2@gmail.com>
2026-04-01 13:03:23 -0500

    more work

--- cllay.lisp
+++ cllay.lisp
@@ -165,25 +165,46 @@
     :exiting))
 
 
-#|
-;;TODO                                        ;
-typedef enum {                          ;
-    CLAY_TRANSITION_PROPERTY_NONE = 0,
-    CLAY_TRANSITION_PROPERTY_X = 1,
-    CLAY_TRANSITION_PROPERTY_Y = 2,
-    CLAY_TRANSITION_PROPERTY_POSITION = CLAY_TRANSITION_PROPERTY_X | CLAY_TRANSITION_PROPERTY_Y,
-    CLAY_TRANSITION_PROPERTY_WIDTH = 4,
-    CLAY_TRANSITION_PROPERTY_HEIGHT = 8,
-    CLAY_TRANSITION_PROPERTY_DIMENSIONS = CLAY_TRANSITION_PROPERTY_WIDTH | CLAY_TRANSITION_PROPERTY_HEIGHT,
-    CLAY_TRANSITION_PROPERTY_BOUNDING_BOX = CLAY_TRANSITION_PROPERTY_POSITION | CLAY_TRANSITION_PROPERTY_DIMENSIONS,
-    CLAY_TRANSITION_PROPERTY_BACKGROUND_COLOR = 16,
-    CLAY_TRANSITION_PROPERTY_OVERLAY_COLOR = 32,
-    CLAY_TRANSITION_PROPERTY_CORNER_RADIUS = 64,
-    CLAY_TRANSITION_PROPERTY_BORDER_COLOR = 128,
-    CLAY_TRANSITION_PROPERTY_BORDER_WIDTH = 256,
-    CLAY_TRANSITION_PROPERTY_BORDER = CLAY_TRANSITION_PROPERTY_BORDER_COLOR | CLAY_TRANSITION_PROPERTY_BORDER_WIDTH
-} Clay_TransitionProperty;
-|#
+(deftype transition-property-normalized ()
+  '(member
+    :none
+    :x
+    :y
+    :width
+    :height
+    :background-color
+    :overlay-color
+    :corner-radius
+    :border-color
+    :border-width))
+
+(deftype transition-property ()
+  '(member
+    :none
+    :x
+    :y
+    :position ;; normalizes to :x :y
+    :width
+    :height
+    :dimensions ;; normalizes to :width :height
+    :bounding-box ;; normalizes to :x :y :width :height
+    :background-color
+    :overlay-color
+    :corner-radius
+    :border-color
+    :border-width
+    :border ;; normalizes to :border-height :border-width
+    ))
+
+
+(defun normalize-transition-properties (properties-list)
+  (loop :for prop :in properties-list
+        :append (case prop
+                   (:position '(:x :y))
+                   (:dimension '(:width :height))
+                   (:bounding-box '(:x :y :width :height))
+                   (:border '(:border-width :border-color))
+                   (otherwise (list prop)))))
 
 (defstruct transition-callback-arguments
   (transition-state nil :type transition-state)
@@ -199,6 +220,11 @@ typedef enum {                          ;
     :skip-on-first-parent-frame
     :trigger-on-first-parent-frame))
 
+(deftype transition-exit-trigger-type ()
+  '(member
+    :skip-when-parent-exits
+    :trigger-when-parent-exits))
+
 (deftype transition-interaction-handling-type ()
   '(member
     :disable-interactions-while-transitioning-position
@@ -210,8 +236,148 @@ typedef enum {                          ;
     :natural-order
     :above-siblings))
 
-(defstruct transition-element-config )
+(deftype transition-properties-list () 'list)
+
+(defstruct transition-element-config-enter
+  set-initial-state;;(function (transition-data transition-properties-list) transition-data)
+  (trigger :skip-on-first-parent-frame :type transition-enter-trigger-type))
+
+(defstruct transition-element-config-exit
+  set-final-state ;;(function (transition-data transition-properties-list) transition-data)
+  (trigger :skip-when-parent-exits :type transition-exit-trigger-type)
+  (sibling-ordering :underneath-siblings :type exit-transition-sibling-ordering)
+  )
+
+(defstruct transition-element-config
+  handler ;; (function (transition-callbacl-arguments) boolean)
+  (duration 0.0f :type single-float)
+  (properties nil :type list)
+  (enter (make-transition-element-config-enter :type transition-element-config-enter))
+  (exit (make-transition-element-config-exit :type transition-element-config-exit)))
+
+(defstruct text-render-data
+  (string-contents "" :type string)
+  (text-color (make-color) :type color)
+  (font-id 0 :type fixnum)
+  (font-size 0 :type fixnum)
+  (letter-spacing 0 :type fixnum)
+  (line-height 0 :type fixnum))
+
+(defstruct rectangle-render-data
+  (background-color (make-color) :type color)
+  (corner-radius (make-corner-radius) :type corner-radius))
+
+(defstruct image-render-data
+  (background-color (make-color) :type color)
+  (corner-radius (make-corner-radius) :type corner-radius)
+  image-data)
+
+(defstruct custom-render-data
+  (background-color (make-color) :type color)
+  (corner-radius (make-corner-radius) :type corner-radius)
+  custom-data)
+
+(defstruct clip-render-data
+  (horizontal nil :type boolean)
+  (vertical nil :type boolean))
+
+(defstruct overlay-color-render-data
+  (color (make-color) :type color))
+
+(defstruct border-render-data
+  (color (make-color) :type color)
+  (corner-radius (make-corner-radius) :type corner-radius)
+  (width (make-border-width) :type border-width))
+
+(deftype render-data ()
+  '(or
+    text-render-data
+    rectangle-render-data
+    image-render-data
+    custom-render-data
+    clip-render-data
+    overlay-color-render-data
+    border-render-data))
+
+(defstruct scroll-container-data
+  (scroll-position (make-vec2) :type vec2)
+  (scroll-container-dimensions (make-dimensions) :type dimensions)
+  (content-dimensions (make-dimensions) :type dimensions)
+  (config (make-clip-element-config) :type clip-element-config)
+  (found nil :type boolean))
+
+(defstruct element-data
+  (bounding-box (make-bounding-box) :type bounding-box)
+  (found nil :type boolean))
+
+(deftype render-command-type ()
+  '(member
+    :none
+    :rectangle
+    :border
+    :text
+    :image
+    :scissor-start
+    :scissor-end
+    :overlay-color-start
+    :overlay-color-end
+    :custom))
+
+(defstruct render-command
+  (bounding-box (make-bounding-box) :type bounding-box)
+  (render-data (make-render-data) :type render-data)
+  userdata
+  (id 0 :type fixnum)
+  (z-index 0 :type fixnum)
+  (command-type :none :type render-command-type))
+
+(deftype pointer-data-interaction-state ()
+  '(member
+    :pressed-this-frame
+    :pressed
+    :released-this-frame
+    :released))
+
+(defstruct pointer-data
+  (position (make-vec2) :type vec2)
+  (state :pressed-this-frame :type pointer-data-interaction-state))
+
+(defstruct element-declaration
+  (layout (make-layout-config) :type layout-config)
+  (background-color (make-color) :type color)
+  (overlay-color (make-color) :type color)
+  (corner-radius (make-corner-radius) :type corner-radius)
+  (aspect-ratio (make-aspect-ratio-element-config) :type aspect-ratio-element-config)
+  (image (make-image-element-config) :type image-element-config)
+  (floating (make-floating-element-config) :type floating-element-config)
+  (custom (make-custom-element-config) :type custom-element-config)
+  (clip (make-clip-element-config) :type clip-element-config)
+  (border (make-border-element-config) :type border-element-config)
+  (transition (make-transition-element-config) :type transition-element-config)
+  userdata)
+
+;; TODO
+;; Clay_LayoutConfig CLAY_LAYOUT_DEFAULT = CLAY__DEFAULT_STRUCT;
+;; Clay_Color Clay__Color_DEFAULT = CLAY__DEFAULT_STRUCT;
+;; Clay_CornerRadius Clay__CornerRadius_DEFAULT = CLAY__DEFAULT_STRUCT;
+;; Clay_BorderWidth Clay__BorderWidth_DEFAULT = CLAY__DEFAULT_STRUCT;
 
 
+(defstruct wrapped-text-line
+  (dimensions (make-dimensions) :type dimensions)
+  (line "" :type string))
 
+(defstruct text-element-data
+  (text "" :type string)
+  (dimensions (make-dimensions) :type dimensions)
+  wrapped-lines ;;list of wrapped-text-line
+  )
 
+(defstruct layout-element-children
+  elements
+  length)
+
+(defstruct layout-element
+  (children (make-layout-element-children) :type layout-element-children)
+  (dimensions (make-dimensions) :type dimensions)
+  (min-dimensiosn (make-dimensions) :type dimensions))
